5ddd9511f8373e3cc5e18911786a0ba585d00cee
Henry T <ton.henry92@gmail.com>
2018-01-30 20:01:37 -0500

    Added last name check for search.

--- search.php
+++ search.php
@@ -6,11 +6,9 @@
 	$username = "cmadmin@contactmanager-db";
 	$password = "5proc!$0oS21";
 	$db = "contact_manager";
-
   $count = 0;
   $search_val = $_POST['search'];
   $current_user = $_POST['current_user'];
-
   // Establishing the connection
   $conn = mysqli_connect($servername, $username, $password, $db);
 	if ($conn->connect_error)
@@ -20,12 +18,11 @@
 	else
 	{
 		// prepared statement to defend against sql injection attacks
-		$sql = $conn->prepare("SELECT * FROM contacts WHERE first_name LIKE CONCAT (?, '%') OR last_name LIKE CONCAT (?, '%') and username_link = ?");
+		$sql = $conn->prepare("SELECT * FROM contacts WHERE (first_name LIKE CONCAT (?, '%') OR last_name LIKE CONCAT (?, '%')) and username_link = ?");
 		$sql->bind_param("sss", $search_val, $search_val, $current_user);
 		$sql->execute();
 		$result = $sql->get_result();
 		$my_arr = array();
-
 		if ($result->num_rows > 0)
 		{
 			while($row = $result->fetch_assoc())
